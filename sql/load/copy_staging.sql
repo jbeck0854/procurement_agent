99aebaab6d944e0fdf86ba2aca9f4f84f30721e4
Jonathan Beck <jonathanbeck@Jonathans-MacBook-Air.local>
2026-03-02 14:45:32 -0500

    Added inventory_demand loader into the copy_staging.sql file

--- sql/load/copy_staging.sql
+++ sql/load/copy_staging.sql
@@ -76,5 +76,9 @@ TRUNCATE TABLE stg_products;
 TRUNCATE TABLE stg_supplier_products;
 \copy stg_supplier_products FROM 'cleaned_data/suppliers_products.csv' WITH (FORMAT csv, HEADER true);
 
+-- synthetic inventory + demand
+TRUNCATE TABLE stg_inventory_demand;
+\copy stg_inventory_demand FROM 'cleaned_data/inventory_demand.csv' WITH (FORMAT csv, HEADER true);
+
 
 
