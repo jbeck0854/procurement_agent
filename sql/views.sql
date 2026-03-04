cce4dd902102731d0525d244eb77971906fb00f5
Jonathan Beck <jonathanbeck@Jonathans-MacBook-Air.local>
2026-03-04 09:46:31 -0500

    added a supplier_risk_profile view to the views table, that prejoins products

--- sql/views.sql
+++ sql/views.sql
@@ -1,3 +1,5 @@
+
+-- View for supplier product pricing profile
 CREATE OR REPLACE VIEW vw_supplier_product_pricing AS
 SELECT
   s.supplier_id,
@@ -15,7 +17,24 @@ FROM dim_supplier s
 JOIN fact_supplier_product_profile f
   ON f.supplier_key = s.supplier_key
 JOIN dim_product p
-  ON p.product_key = f.product_key
+  ON p.product_key = s.product_key
 LEFT JOIN fact_tariff_schedule_yearly t
   ON t.hts8 = f.hts8;
-LEFT JOIN dim_tariff_code ON dt.hts8 = f.htsq;
+LEFT JOIN dim_tariff_code dt ON dt.hts8 = f.hts8;
+
+-- View for supplier risk profile
+CREATE OR REPLACE VIEW vw_supplier_risk_profile AS
+SELECT 
+  s.supplier_id,
+  s.country_code,
+  p.product,
+
+  s.lead_time_mean,
+  s.lead_time_variance,
+  s.disruption_probability,
+  s.compliance_eligibility,
+  s.logistics_reliability
+
+FROM dim_supplier s 
+LEFT JOIN dim_product p
+  ON p.product_key = s.product_key;
